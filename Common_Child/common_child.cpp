7b439ceda187f4963a1654d80a67c96762cabcb4
Guillaume Militello <guillaume.militello@gmail.com>
2015-08-09 11:10:54 +0800

    Method using a match matrix

--- Common_Child/common_child.cpp
+++ Common_Child/common_child.cpp
@@ -1,69 +1,59 @@
 /*
  * common_child.cpp
  *
- *  Created on: 8 Aug 2015
- *      Author: guillaume
+ *  Created on: 9 Aug 2015
+ *      Author: Guillaume Militello
  */
 
+#include <cmath>
 #include <cstdio>
 #include <iostream>
 #include <fstream>
-#include <cstring>
+#include <string>
 #include <cstdlib>
-#include <cstdio>
-using namespace std;
 
-string s0, s1;
+using namespace std;
 
 ifstream fin ("input.txt");
 ofstream fout ("output.txt");
 
-int max_common_child(unsigned int i, unsigned int j)
-{
-	if(i == s0.size() && j == s1.size())
-	{
-		return 0;
-	}
-	else
-	{
-		unsigned int max=0;
-		unsigned int imax = s0.size();
-		unsigned int jmax = s1.size();
+int main() {
 
-		for(unsigned int _i = i; _i < imax; ++_i)
-		{
-			for(unsigned int _j = j; _j < jmax; ++_j)
-			{
-				if(_i > 0 && _j > 0 && s0.at(_i) == s1.at(_j))
-				{
-					// find in the string with new coordinates
-					unsigned int m = max_common_child(_i+1,_j+1) + 1;
-					if(m > max) max = m;
-
-					// define new boundaries
-					imax = s0.size()-max;
-					jmax = _j;
-					break;
-				}
-			}
-		}
-		return max;
-	}
-}
-
-int main()
-{
-
+	string s0, s1;
 	getline(fin, s0);
 	getline(fin, s1);
 
-	// search in the string from original coordinates
-	cout << max_common_child(0, 0) << endl;
+	int** mat = new int*[s1.size() + 1];
+	for (unsigned int i = 0; i < s1.size() + 1; ++i)
+	{
+		mat[i] = new int[s0.size() + 1]();
+	}
 
-	return 0;
+    // fill a matrix of s0 s1 matches
+    for(unsigned int i = 0; i < s0.size(); ++i)
+	{
+		for(unsigned int j = 0; j < s1.size(); ++j)
+		{
+			if(s0.at(i) == s1.at(j))
+			{
+				// add the match to the next match
+				mat[i+1][j+1] = mat[i][j] + 1;
+			}
+			else
+			{
+				// get the maximum previous paths
+				mat[i+1][j+1] = max(mat[i][j+1], mat[i+1][j]);
+			}
+		}
+	}
+
+	fout << mat[s0.size()][s1.size()];
+
+	for (unsigned int i = 0; i < s1.size() + 1; ++i)
+	{
+		delete[] mat[i];
+	}
+	delete[] mat;
+
+    return 0;
 }
-
-
-
-
-
